34a06e1329087e43a81038dd9718234ce6ecabeb
peacefullmind <tj986222908@gmail.com>
2021-10-13 09:29:42 +0800

    修改twitter

--- twitter/main.py
+++ twitter/main.py
@@ -8,3 +8,15 @@ chromeDriver = "D:\\chromedriver_win32\\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chromeDriver)
 url='https://twitter.com/fuchsiadunlop'
 driver.get(url)
+
+time.sleep(40)
+
+html=driver.page_source
+soup=bs(html, 'html.parser')
+
+#'span',
+spans=soup.find_all("span",class_="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0")
+
+for span in spans:
+    print(span.text)
+
